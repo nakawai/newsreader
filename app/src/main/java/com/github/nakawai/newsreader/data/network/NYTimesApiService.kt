dcd786f01efb7d2b4433563f86860ca2f976fd55
Naoki Kawai <n.kawai@ldi.co.jp>
2020-08-29 17:00:13 +0900

    change retrofit method to suspend

--- app/src/main/java/com/github/nakawai/newsreader/data/network/NYTimesApiService.kt
+++ app/src/main/java/com/github/nakawai/newsreader/data/network/NYTimesApiService.kt
@@ -2,6 +2,7 @@ package com.github.nakawai.newsreader.data.network
 
 import com.github.nakawai.newsreader.data.network.response.TopStoriesResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,8 +12,8 @@ import retrofit2.http.Query
  */
 interface NYTimesApiService {
     @GET("svc/topstories/v2/{section}.json")
-    fun topStories(
+    suspend fun topStories(
         @Path("section") section: String,
         @Query(value = "api-key", encoded = true) apiKey: String
-    ): Call<TopStoriesResponse>
+    ): Response<TopStoriesResponse>
 }
