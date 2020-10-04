9719e8cad072423e84b83f4dd08ae27cc1431079
nakawai <kawai.telem@gmail.com>
2020-10-04 15:51:55 +0900

    Remove unused imports

--- app/src/main/java/com/github/nakawai/newsreader/domain/datasource/AppLocalDataSource.kt
+++ app/src/main/java/com/github/nakawai/newsreader/domain/datasource/AppLocalDataSource.kt
@@ -1,10 +1,6 @@
 package com.github.nakawai.newsreader.domain.datasource
 
-import androidx.lifecycle.LiveData
-import com.github.nakawai.newsreader.data.network.response.StoryResponseItem
 import com.github.nakawai.newsreader.domain.entities.Section
-import com.github.nakawai.newsreader.domain.entities.Story
-import com.github.nakawai.newsreader.domain.entities.StoryUrl
 
 interface AppLocalDataSource {
     fun canCallApi(section: Section): Boolean
