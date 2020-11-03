3a91bf5ae78c94248ecd7fd44c76cbd9aa5bfb71
nakawai <kawai.telem@gmail.com>
2020-11-03 19:02:38 +0900

    Add SearchActivity

--- app/src/main/java/com/github/nakawai/newsreader/presentation/sections/SectionsActivity.kt
+++ app/src/main/java/com/github/nakawai/newsreader/presentation/sections/SectionsActivity.kt
@@ -1,12 +1,18 @@
 package com.github.nakawai.newsreader.presentation.sections
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
+import com.github.nakawai.newsreader.R
 import com.github.nakawai.newsreader.databinding.ActivitySectionsBinding
 import com.github.nakawai.newsreader.domain.entities.Section
 import com.github.nakawai.newsreader.presentation.articles.ArticlesActivity
+import com.github.nakawai.newsreader.presentation.search.SearchActivity
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SectionsActivity : AppCompatActivity(), SectionListAdapter.OnItemClickListener {
@@ -33,6 +39,24 @@ class SectionsActivity : AppCompatActivity(), SectionListAdapter.OnItemClickList
         viewModel.start()
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_sections, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_item_search -> {
+                startActivity(Intent(this, SearchActivity::class.java))
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     private fun observeViewModel() {
