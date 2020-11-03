a73ea2e8894f34c0b7d8449abd16a5a8aa8c7685
nakawai <kawai.telem@gmail.com>
2020-11-03 19:49:02 +0900

    Add SearchView

--- app/src/main/java/com/github/nakawai/newsreader/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/github/nakawai/newsreader/presentation/search/SearchActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.viewpager.widget.ViewPager
 import com.github.nakawai.newsreader.R
 import com.github.nakawai.newsreader.databinding.ActivitySearchBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
+import timber.log.Timber
 
 class SearchActivity : AppCompatActivity() {
 
@@ -19,6 +21,8 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
@@ -35,6 +39,25 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_search, menu)
+
+        val searchView = menu.findItem(R.id.menu_item_search_view).actionView as SearchView
+        searchView.isIconified = false
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Timber.d(newText)
+                return false
+            }
+
+        })
+        return true
+    }
+
+    override fun onNavigateUp(): Boolean {
+        finish()
         return true
     }
 }
