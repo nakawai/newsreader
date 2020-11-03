6c5000d968510ee669b3eba291e1dd0decad874c
nakawai <kawai.telem@gmail.com>
2020-11-03 19:14:22 +0900

    Add SearchActivity

--- app/src/main/java/com/github/nakawai/newsreader/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/github/nakawai/newsreader/presentation/search/SearchActivity.kt
@@ -1,9 +1,12 @@
 package com.github.nakawai.newsreader.presentation.search
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.github.nakawai.newsreader.R
+import com.github.nakawai.newsreader.databinding.ActivitySearchBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
@@ -12,8 +15,10 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        val binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
@@ -25,5 +30,11 @@ class SearchActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        return true
     }
 }
