e008f4a5d624b65cd41320bc342ce9465276d29b
nakawai <kawai.telem@gmail.com>
2021-01-09 22:24:18 +0900

    refactor

---
+++ app/src/main/java/com/github/nakawai/newsreader/data/network/response/topstories/StoryResponseItemTranslatorExtension.kt
@@ -0,0 +1,21 @@
+package com.github.nakawai.newsreader.data.network.response.topstories
+
+import com.github.nakawai.newsreader.data.DataTranslator
+import com.github.nakawai.newsreader.domain.entities.Article
+import com.github.nakawai.newsreader.domain.entities.ArticleUrl
+import com.github.nakawai.newsreader.domain.entities.Multimedia
+import com.github.nakawai.newsreader.domain.entities.Section
+
+
+fun StoryResponseItem.translate(): Article = Article(
+    title = this.title.orEmpty(),
+    storyAbstract = this.storyAbstract.orEmpty(),
+    url = ArticleUrl(this.url.orEmpty()),
+    multimediaUrlList = this.multimedia?.map { Multimedia(it.url.orEmpty()) } ?: emptyList(),
+    publishedDate = this.publishedDate?.let { StoryResponseItem.DATE_FORMAT.parse(it) },
+    updatedDate = this.updatedDate?.let { StoryResponseItem.DATE_FORMAT.parse(it) },
+    // FIXME remove property
+    isRead = false,
+    section = Section.values().find { DataTranslator.translate(it).value == this.section }
+        ?: throw IllegalArgumentException("invalid apiSection:${this.section}"),
+)
