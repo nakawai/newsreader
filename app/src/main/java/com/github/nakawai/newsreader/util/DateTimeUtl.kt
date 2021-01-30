90ef72155b2c2057cd53bbe503a7e42f8ed52690
nakawai <kawai.telem@gmail.com>
2021-01-30 19:29:08 +0900

    add test

--- app/src/main/java/com/github/nakawai/newsreader/util/DateTimeUtl.kt
+++ app/src/main/java/com/github/nakawai/newsreader/util/DateTimeUtl.kt
@@ -1,5 +1,6 @@
 package com.github.nakawai.newsreader.util
 
+import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -13,5 +14,10 @@ object DateTimeUtl {
     fun parseToZonedDateTime(dateText: String): ZonedDateTime? {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ")
         return ZonedDateTime.from(formatter.parse(dateText))
+    }
+
+    fun parseToLocalDateYyyyMmDd(dateText: String): LocalDate? {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        return runCatching { LocalDate.from(formatter.parse(dateText)) }.getOrNull()
     }
 }
--- app/src/test/java/com/github/nakawai/newsreader/util/DateTimeUtlTest.kt
+++ app/src/test/java/com/github/nakawai/newsreader/util/DateTimeUtlTest.kt
@@ -4,7 +4,9 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import java.time.ZoneId
 import java.time.ZoneOffset
+import java.util.*
 
 class DateTimeUtlTest {
 
@@ -32,6 +34,7 @@ class DateTimeUtlTest {
         assertThat(actual.minute).isEqualTo(30)
         assertThat(actual.second).isEqualTo(38)
         assertThat(actual.offset).isEqualTo(ZoneOffset.ofHours(-4))
+
     }
 
     @Test
@@ -50,5 +53,41 @@ class DateTimeUtlTest {
         assertThat(actual.minute).isEqualTo(30)
         assertThat(actual.second).isEqualTo(38)
         assertThat(actual.offset).isEqualTo(ZoneOffset.ofHours(-4))
+        assertThat(actual.zone).isEqualTo(ZoneId.of("-04:00"))
     }
+
+    @Test
+    fun testYYYYMMDDParseZonedDateTime() {
+        // Arrange
+        val dateText = "2020-10-03"
+
+        // Act
+        val actual = DateTimeUtl.parseToLocalDateYyyyMmDd(dateText)!!
+
+        // Assert
+        assertThat(actual.year).isEqualTo(2020)
+        assertThat(actual.monthValue).isEqualTo(10)
+        assertThat(actual.dayOfMonth).isEqualTo(3)
+    }
+
+    @Test
+    fun hoge() {
+
+        // Act
+        val tokyo = TimeZone.getTimeZone("Asia/Tokyo")
+        val utc = TimeZone.getTimeZone("UTC")
+
+
+        val c1 = Calendar.getInstance(tokyo).also {
+            it.set(2020, 1, 1, 0, 0, 0)
+        }
+
+        val c2 = Calendar.getInstance(utc).also {
+            it.set(2020, 1, 1, 0, 0, 0)
+        }
+        // Assert
+        assertThat(c1.time.time).isEqualTo(c2.time.time)
+
+    }
+
 }
